c6b2d228e4fb758cf8b7dc5ae3838ab998559da2
Kenneth Lausdahl <kenneth@lausdahl.com>
2022-12-12 22:59:59 +0100

    fixed missing handler registration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-VSS.init({ usePlatformScripts: true, explicitNotifyLoaded: true });
+// VSS.init({ usePlatformScripts: true, explicitNotifyLoaded: true });
 
 import witClient = require("TFS/WorkItemTracking/RestClient");
 import { urlHelper } from "VSS/Locations";
@@ -11,68 +11,73 @@ import Clipboard = require("VSS/Utils/Clipboard");
 import { htmlEncode } from "VSS/Utils/String";
 import { Uri } from "VSS/Utils/Url";
 
+VSS.ready(function () {
 
+    var extensionCtx = VSS.getExtensionContext();
 
-var extensionCtx = VSS.getExtensionContext();
+    VSS.register(extensionCtx.publisherId + "." + extensionCtx.extensionId + ".contextMenu", {
+        execute: (actionContext) => {
+            let workItemId = actionContext.id
+                || actionContext.workItemId
+                || (actionContext.ids && actionContext.ids.length > 0 && actionContext.ids[0])
+                || (actionContext.workItemIds && actionContext.workItemIds.length > 0 && actionContext.workItemIds[0]);
 
-VSS.register(extensionCtx.publisherId + "." + extensionCtx.extensionId + ".contextMenu", {
-    execute: (actionContext) => {
-        let workItemId = actionContext.id
-            || actionContext.workItemId
-            || (actionContext.ids && actionContext.ids.length > 0 && actionContext.ids[0])
-            || (actionContext.workItemIds && actionContext.workItemIds.length > 0 && actionContext.workItemIds[0]);
-
-        if (workItemId) {
-
-
-
-
-            //alert(workItemId);
-
-            client.getWorkItem(workItemId).then(function (workItem) {
-
-                if (workItem) {
-
-                    let name = workItem.fields["System.Title"];
-                    let branch = "AB#" + workItemId + "-" + name.replace(/ /g, '_').replace(/[^a-z0-9_]/gi, '');
-
-                    Clipboard.copyToClipboard(branch, <Clipboard.IClipboardOptions>{
-                        copyAsHtml: false,
-                        showCopyDialog:true
-                    });
-
-                    console.log(branch);
-                    //alert(branch);
-
-                  
-                    VSS.getService<IHostDialogService>(VSS.ServiceIds.Dialog).then(function(dialogService) {
-                        var extensionCtx = VSS.getExtensionContext();
-                        // Build absolute contribution ID for dialogContent
-                        var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".branching-form";
-                
-                        // Show dialog
-                        var dialogOptions = {
-                            title: "Workitem Branch Name",
-                            width: 400,
-                            height: 300,
-                            buttons: null,
-                            urlReplacementObject: { branch: encodeURIComponent( branch) }
-                        };
-                
-                        dialogService.openDialog(contributionId, dialogOptions);
-                    });
+            if (workItemId) {
 
 
 
 
-                }
+                //alert(workItemId);
 
-            });
+                client.getWorkItem(workItemId).then(function (workItem) {
 
-            // var text = "Example text to appear on clipboard";
+                    if (workItem) {
 
+                        let name = workItem.fields["System.Title"];
+                        let branch = "AB#" + workItemId + "-" + name.replace(/ /g, '_').replace(/[^a-z0-9_]/gi, '');
+
+                        Clipboard.copyToClipboard(branch, <Clipboard.IClipboardOptions>{
+                            copyAsHtml: false,
+                            showCopyDialog: true
+                        });
+
+                        console.log(branch);
+                        //alert(branch);
+
+
+                        VSS.getService<IHostDialogService>(VSS.ServiceIds.Dialog).then(function (dialogService) {
+                            var extensionCtx = VSS.getExtensionContext();
+                            // Build absolute contribution ID for dialogContent
+                            var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".branching-form";
+
+                            // Show dialog
+                            var dialogOptions = {
+                                title: "Workitem Branch Name",
+                                width: 400,
+                                height: 300,
+                                buttons: null,
+                                urlReplacementObject: { branch: encodeURIComponent(branch) }
+                            };
+
+                            dialogService.openDialog(contributionId, dialogOptions);
+                        });
+
+
+
+
+                    }
+
+                });
+
+                // var text = "Example text to appear on clipboard";
+
+            }
         }
-    }
+    });
+
+    VSS.notifyLoadSucceeded();
 });
+
+
 
 
